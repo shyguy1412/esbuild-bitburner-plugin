4c900c117f32ffbf3351c43951961e558ff17a38
Nils Ramstöck <nilsramstoeck@gmail.com>
2023-10-05 13:40:27 +0200

    add RemoteApiServer class

---
+++ RemoteApiServer.js
@@ -0,0 +1,158 @@
+const {log} = require('console');
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+class RemoteApiServer extends WebSocketServer{
+
+  constructor(){
+    super({
+      httpServer: http.createServer((request, response) => {
+        response.writeHead(404);
+        response.end();
+      }),
+      autoAcceptConnections: false,
+      maxReceivedMessageSize: 1.49e+7,
+      maxReceivedFrameSize: 1.49e+7
+    });
+
+    this.queue = new Map();
+  }
+
+  listen(port, callback){
+
+    this.config.httpServer[0].listen(port, callback);
+
+    this.on('request', async (request) => {
+      if(this.connection && this.connection.readyState == 2){
+        request.reject(400, "Only one client can connect at a time")
+        return;
+      }
+      
+      this.connection = request.accept(null, request.origin);
+
+      this.connection.on('message', (e) => {
+        const response = JSON.parse(e.utf8Data);
+        if(this.queue.has(response.id)){
+          this.queue.get(response.id)(response);
+          this.queue.delete(response.id);
+        }
+      });
+      
+      this.emit('client-connected');
+     
+    });
+  }
+
+  write(obj){
+    return new Promise((resolve, reject) => {
+      if(!this.connection || !this.connection.connected){
+        reject("No open connection");
+        return;
+      }
+      const id = Date.now();
+      const message = JSON.stringify({
+        jsonrpc: "2.0",
+        id,
+         ...obj 
+      });
+      
+      this.queue.set(id, resolve);
+
+      this.connection.send(message);
+    });
+  }
+
+  getDefinitionFile(){
+    return this.write({
+      method: "getDefinitionFile"
+    });
+  }
+
+  pushFile({filename, content, server}){
+    console.log({filename, content, server});
+    return this.write({
+      method: "pushFile",
+      params: {
+        filename,
+        content,
+        server
+      }
+    });
+  }
+
+  getFile({filename, server}){
+    return this.write({
+      method: "getFile",
+      params: {
+        filename,
+        server,
+      }
+    });
+  }
+ 
+  getFileNames({server}){
+    return this.write({
+      method: "getFileNames",
+      params: {
+        server,
+      }
+    });
+  }
+
+  getAllFiles({server}){
+    return this.write({
+      method: "getAllFiles",
+      params: {
+        server,
+      }
+    });
+  }
+   
+  deleteFile({filename, server}){
+    return this.write({
+      method: "deleteFile",
+      params: {
+        filename,
+        server,
+      }
+    });
+  }
+
+  calculateRAM({filename, server}){
+    return this.write({
+      method: "calculateRAM",
+      params: {
+        filename,
+        server,
+      }
+    });
+  }
+
+}
+
+module.exports = RemoteApiServer;
+
+      async function pushFiles(files){
+        return Promise.all(files.map(async file => RemoteAPI.write({
+            "method": "pushFile",
+            "params": {
+              filename: file.name,
+              content: file.content,
+              server: file.server,
+            }
+        })));
+        for(const file of files){
+          await RemoteAPI.write({
+          });
+        }
+      }
+
+      async function calculateRAM(files){
+        return await Promise.all(files.map(async file => ({
+          name: file.name,
+          server: file.server,
+          cost: await RemoteAPI.write()
+        })));
+      }
+
+
